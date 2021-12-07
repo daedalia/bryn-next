869c18f91e40ebf46489aefef619b09dc1eca4ee
daedalia <bryn@daedalia.co.uk>
2021-12-07 09:27:57 +0000

    Initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Layout } from '../coponents/Layout'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return <Layout><Component {...pageProps} /></Layout>
 }
 
 export default MyApp
