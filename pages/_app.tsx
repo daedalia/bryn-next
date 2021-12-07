b5b52eac5578aa0718e49286fda176517066359c
daedalia <bryn@daedalia.co.uk>
2021-12-07 10:26:44 +0000

    Fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import { Layout } from '../components/Layout'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Layout><Component {...pageProps} /></Layout>
 }
 
